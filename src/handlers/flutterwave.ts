ed29f01f5669d7245f08b30016c774e4b3112c36
Timmy Omolana <timmieomolanakiel@gmail.com>
2022-07-04 18:33:07 +0100

    Update: create card payment handler and add event dispatcher to webhook

--- src/handlers/flutterwave.ts
+++ src/handlers/flutterwave.ts
@@ -1,7 +1,10 @@
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
 // import Flutterwave from 'flutterwave-node-v3';
 import { FLW_API_URL, FLW_SEC_KEY, FLW_WBHK_HASH } from '../config/constants';
 import axios from 'axios';
 import logger from '../config/logger';
+import { EventDispatcher } from 'event-dispatch';
+import './events/eventSubscriber';
 // const flw = new Flutterwave(FLW_PUB_KEY, FLW_SEC_KEY);
 
 type Acct = {
@@ -47,6 +50,53 @@ const generateAccountNo = async (acct: any) => {
 //   }
 // };
 
+// interface CardAuth {
+//   mode: string
+
+// }
+interface CardPayment {
+  card_number: string;
+  cvv: string;
+  expiry_month: string;
+  expiry_year: string;
+  currency: string;
+  amount: string;
+  email: string;
+  fullname: string;
+  tx_ref: string;
+  redirect_url: string;
+}
+
+const fundWithCard = async (payload: CardPayment) => {
+  try {
+    const data: any = await axios
+      .post(
+        `${FLW_API_URL}/charges?type=card`,
+        {
+          ...payload,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${FLW_SEC_KEY}`,
+          },
+        },
+      )
+      .then((res) => res.data)
+      .catch((err: any) => {
+        logger.error(err.message);
+        throw new Error(err);
+      });
+
+    logger.info(data);
+    return data.status === 'success' ? data.data : null;
+  } catch (err: any) {
+    throw new Error(err);
+  }
+};
+
+const eventDispatcher = new EventDispatcher();
+
 export const verifyWebhook = (req: any, res: any) => {
   const signature = req.headers['verif-hash'];
 
@@ -56,10 +106,13 @@ export const verifyWebhook = (req: any, res: any) => {
     logger.warn('Something fishy 🐟  might be going on...');
     return res.status(401).end();
   }
+  // emit wegevent and handle it
+  eventDispatcher.dispatch(req.body.event, req.body.data);
   logger.info('Successfully verified webhook request');
   return res.status(200).end();
 };
 
 export default {
   generateAccountNo,
+  fundWithCard,
 };
