58a4d34748ee4cfc685e710c5c5cedbcdf5c50d6
Timmy Omolana <timmieomolanakiel@gmail.com>
2022-07-04 18:30:36 +0100

    Update: add new variable FLWENCTYPE

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv';
-dotenv.config();
 
+dotenv.config();
 export const { PORT } = process.env;
 export const { NODE_ENV } = process.env;
 export const { API_PATH } = process.env;
 export const { FLW_PUB_KEY } = process.env;
 export const { FLW_SEC_KEY } = process.env;
+export const { FLW_ENC_KEY } = process.env;
 export const { FLW_API_URL } = process.env;
 export const { FLW_WBHK_HASH } = process.env;
